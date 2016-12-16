70ef6f17d654eb0ca0698bcfbd653aaff1d90184
binghuan <binghuanlin@gmail.com>
2016-12-16 18:27:40 +0800

    Add Google analytics.

---
+++ sample.js
@@ -0,0 +1,11 @@
+var input = {
+    cardType: 1,
+    pageData = [{
+            question: "Are you maik?",
+            options: ["yes", "no"]
+        }, {
+            question: "Who the hell are you?",
+            options: ["I am a man", "WTF?"]
+        }
+    ]
+}
