1c92f67e9cdb4e3f341f1ec2e90caa6a346c4b3c
BH_Lin <binghuanlin@gmail.com>
2016-12-23 11:14:15 +0800

    Add new question and options.

---
+++ sample3.js
@@ -0,0 +1,17 @@
+var input = {
+    pageData: [
+    {
+        question: "Do you remember any of the ads you’ve seen here before?",
+        options: ["Very High", "Well", "Kind of", "Not really", "Not at all"]
+    },
+    {
+        question: "How do you view the brands advertised?",
+        //options: ["yes", "no", "IDK", "!!"]
+        options: ["Very Favorably", "Favorably", "Neutral", "Unfavorably", "Very Unfavorably"]
+    },
+    {
+        question: "How likely are you to buy a product shown here in the next 2 months?",
+        options: ["Definitely will","Probably will", "May or may not", "Probably will not", "Definitely will not"]
+    }
+    ]
+}
